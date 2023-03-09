8701cccec10356a905fbade7afd3f735f6b8453e
nitin35byte <akhileshc181@gmail.com>
2023-03-09 22:58:45 +0530

    Initial commit

---
+++ crud.py
@@ -0,0 +1,13 @@
+import streamlit as st
+#from dbhelper import DB
+#import plotly.graph_objects as go
+#import plotly.express as px
+
+#db = DB()
+
+st.sidebar.title('Flights Analytics')
+
+user_option = st.sidebar.selectbox('Menu',['Select One','Check Flights','Analytics'])
+
+if user_option == 'Check Flights':
+    st.title('Check Flights')
